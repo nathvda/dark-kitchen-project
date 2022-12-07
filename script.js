6defcda71a394a571d99b7daed3e9fe2157b714d
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2022-12-07 15:41:57 +0100

    clairsombre

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ let darkmodeBouton = document.querySelector(".darkmode");
 
 function turnDark(){
     let cssTarget = document.getElementById('styleToggle');
-    
-    cssTarget.href = 'darkmode.css';
+   if (cssTarget.href == "style.css"){
+    console.log('clair');
+   } else if(cssTarget.href == "darkmode.css"){
+   console.log('fonce');
+  cssTarget.href = 'darkmode.css';
+   }
 }
+
+
 darkmodeBouton.addEventListener("click", turnDark);
