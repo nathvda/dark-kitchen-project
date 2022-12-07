1c8a7dcc87e7a4dd518d397d84128e3fb9a36fd1
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2022-12-07 10:30:52 +0100

    Removing console logs

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
-
-
-
 // Filtre les patisseries
 function filterPastries(){
     let filtresPatisserie = document.querySelector('input[name="patisserie"]:checked').value;
+<<<<<<< HEAD
     console.log(filtresPatisserie);
     let itemsToHide =  document.querySelectorAll(`.${filtresPatisserie}`);
     console.log(itemsToHide);
+=======
+    let patisserieMenu = document.querySelector(`.patisseries__wrapper`);
+    let itemsToHide =  patisserieMenu.querySelectorAll(`div`);
+
+>>>>>>> a2f276a (Removing console logs)
     for (el of itemsToHide){
         el.classList.toggle('hidePastry');
     }
 }
 
+<<<<<<< HEAD
 let filtreswrap = document.getElementById('patisserie_filters');
 console.log(filtreswrap);
 
+=======
+}
+
+let filtreswrap = document.querySelector('.patisserie_filters');
+
+// Déclenche les filtres
+filtreswrap.addEventListener('change', filterPastries);
+
+>>>>>>> a2f276a (Removing console logs)
 
 
 
